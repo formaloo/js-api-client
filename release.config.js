8b928e2a4fcf9d31b5aa70b72d7e2157fbe7b84b
Amir Hashemi <amir.hashemei@gmail.com>
2021-04-25 17:53:14 +0430

    ci: modify semantic-release plugin settings
    include only build files in github like npm release
    and also generate and include changelog.md

--- release.config.js
+++ release.config.js
@@ -2,7 +2,18 @@ module.exports = {
   "plugins": [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/npm",
-    "@semantic-release/github",
+    ["@semantic-release/changelog", {
+      "changelogFile": "CHANGELOG.md"
+    }],
+    ["@semantic-release/git", {
+      "assets": ["CHANGELOG.md"]
+    }],
+		["@semantic-release/npm", {
+      "npmPublish": true,
+			"tarballDir": "dist"
+		}],
+    ["@semantic-release/github", {
+      "assets": ["dist/**"]
+    }],
   ]
 }
