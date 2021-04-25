22cd4b11be0fe7803027283417ed39a952b4589f
Amir Hashemi <amir.hashemei@gmail.com>
2021-04-25 12:50:14 +0430

    test: add simple unit test for customer creation

---
+++ tests/unit/customers.test.ts
@@ -0,0 +1,38 @@
+
+import { axios } from '../common'
+import { clientConstructor } from '../../src/api-client/client'
+import { API_URL } from '../../src/constants'
+import { Customers } from '../../src/api-client/resources'
+
+beforeEach(() => {
+  axios.reset()
+  axios.onAny().reply(200)
+})
+
+const constants = {
+  baseUrl: API_URL,
+  apiKey: 'apiKey',
+  apiSecret: 'apiSecret',
+  token: 'token',
+  writeKey: 'writeKey',
+}
+
+const http = clientConstructor({
+  apiKey: constants.apiKey,
+  apiSecret: constants.apiSecret,
+  writeKey: constants.writeKey
+})
+
+const customerRequest = new Customers(http)
+
+test('customer create sends correct request', async () => {
+  const data = JSON.stringify({"key":"value"})
+  await customerRequest.create({
+    token: constants.token,
+    data,
+  })
+
+  expect(axios.history.post[0].url).toBe(`${constants.baseUrl}/customers/`)
+  expect(axios.history.post[0].data).toBe(data)
+
+})
