07ccbba4e4c47d99819ddd80399fb6d796bcbee2
Amir Hashemi <amir.hashemei@gmail.com>
2021-04-26 12:03:21 +0430

    feat(type): add base types to improve type-hinting and DX
    add base request utility types plus base Formaloo types:
    customer, tag, activity and a few utility types to improve
    true typing support and leverage editor hints

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,8 @@
+export type RequireAtLeastOne<T> = {
+  [K in keyof T]-?: Required<Pick<T, K>> &
+    Partial<Pick<T, Exclude<keyof T, K>>>;
+}[keyof T];
+
 /**
  * Generic object document.
  */
@@ -25,8 +30,14 @@ export interface ClientArg extends ObjectData {
 /**
  * Formaloo request data object.
  */
-
-export interface RequestData extends ObjectData {
+export type BaseWriteRequestArgs = RequireAtLeastOne<{
+  token?: string;
+  useWriteKey?: boolean;
+}>;
+export interface BaseReadRequestArgs {
+  token: string;
+}
+export interface RequestArgs extends ObjectData {
   data?: unknown;
   argsHeaders?: ObjectData;
   params?: ObjectData;
@@ -35,28 +46,123 @@ export interface RequestData extends ObjectData {
 }
 
 /**
- * Formaloo update request object.
- */
-export interface UpdateRequestData extends RequestData {
-  slug: string;
-}
-
-/**
  * Formaloo list request object.
  */
-export interface ListRequestData extends ObjectData {
-  token: string;
-  params?: ObjectData;
+export interface BaseListRequestParams {
   search?: string;
   page?: number;
   page_size?: number;
-  argsHeaders?: ObjectData;
+}
+export interface ListRequestArgs extends BaseReadRequestArgs {
+  params?: BaseListRequestParams & ObjectData;
 }
 
 /**
  * Formaloo request object.
  */
-export interface Request extends RequestData {
+export interface Request extends RequestArgs {
   url: string;
   method: string;
+}
+
+export interface PaginatedListMetaData {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  page_size: number;
+  page_count: number;
+  current_page: number;
+}
+
+export interface ResponseError {
+  errors: {
+    general_errors: string[];
+    form_errors: { [key: string]: string[] };
+  };
+}
+
+/**
+ *
+ * @export
+ * @interface Tag
+ */
+export interface Tag {
+  readonly slug: string;
+  title: string;
+  description?: string;
+}
+
+/**
+ *
+ * @export
+ * @interface Customer
+ */
+export interface Customer {
+  readonly code: string;
+  customer_data?: ObjectData[];
+  full_name?: string | null;
+  email?: string;
+  phone_number?: string | null;
+  tags?: Partial<Tag>[];
+  readonly score?: number;
+  readonly created_at?: string;
+  readonly updated_at?: string;
+}
+
+/**
+ * @export
+ * @enum {string}
+ */
+export enum OperationType {
+  Create = "create",
+  Read = "read",
+  Update = "update",
+  Delete = "delete",
+}
+
+/**
+ * ActivityRelationInstance
+ */
+interface ActivityRelationInstance {
+  identifier_value: string | number;
+  title?: string;
+  instance_data: ObjectData;
+  entity: {
+    readonly slug: string;
+    model_name: string;
+    title?: string | null;
+    description?: string | null;
+  };
+}
+
+/**
+ * @export
+ * @interface ActivityRelation
+ */
+interface ActivityRelation {
+  relation_name: string;
+  instance: ActivityRelationInstance;
+}
+
+/**
+ * @export
+ * @interface Activity
+ */
+export interface Activity {
+  readonly slug: string;
+  action: string;
+  description?: string;
+  customer: RequireAtLeastOne<{
+    code?: string;
+    email?: string;
+    phone_number?: string;
+  }>;
+  activity_data: ObjectData;
+  relations?: ActivityRelation[];
+  currency?: string;
+  monetary_value?: unknown;
+  activity_date?: Date | string;
+  readonly created_at: string;
+  readonly updated_at: string;
+  operation_type?: OperationType;
 }
