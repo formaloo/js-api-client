ba748e40f3fa9b567000eac6cd1ebbe4eccb8dd8
Amir Hashemi <amir.hashemei@gmail.com>
2021-04-27 15:00:57 +0430

    feat: define activity related types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,12 +1,17 @@
 import { Method } from "axios";
+import { HttpStatusCode } from "src/utils/network";
 
 export type RequireAtLeastOne<T> = {
   [K in keyof T]-?: Required<Pick<T, K>> &
     Partial<Pick<T, Exclude<keyof T, K>>>;
 }[keyof T];
 
+export type EmptyObject = Record<string, never>;
 /**
- * Generic object document.
+ * Generic Object
+ *
+ * @export
+ * @interface ObjectData
  */
 export interface ObjectData {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -14,14 +19,22 @@ export interface ObjectData {
 }
 
 /**
- * HTTP Client for API requests.
+ *
+ * @export
+ * @interface HTTPClient
  */
 export interface HTTPClient {
-  request: (args: Request) => Promise<unknown>;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  request: (args: Request) => Promise<any>;
 }
 
 /**
+ *
  * Argument object for Formaloo API client
+ *
+ * @export
+ * @interface ClientArg
+ * @extends {ObjectData}
  */
 export interface ClientArg extends ObjectData {
   apiKey?: string;
@@ -36,9 +49,24 @@ export type BaseWriteRequestArgs = RequireAtLeastOne<{
   token?: string;
   useWriteKey?: boolean;
 }>;
+
+/**
+ *
+ *
+ * @export
+ * @interface BaseReadRequestArgs
+ */
 export interface BaseReadRequestArgs {
   token: string;
 }
+
+/**
+ *
+ *
+ * @export
+ * @interface RequestArgs
+ * @extends {ObjectData}
+ */
 export interface RequestArgs extends ObjectData {
   data?: unknown;
   argsHeaders?: ObjectData;
@@ -76,6 +104,9 @@ export interface PaginatedListMetaData {
   current_page: number;
 }
 
+export interface ResponseData<T> {
+  data: T;
+}
 export interface ResponseError {
   errors: {
     general_errors: string[];
@@ -83,7 +114,12 @@ export interface ResponseError {
   };
 }
 
+export interface Response<T> extends ResponseData<T>, ResponseError {
+  status: HttpStatusCode;
+}
+
 /**
+ *
  *
  * @export
  * @interface Tag
@@ -95,6 +131,7 @@ export interface Tag {
 }
 
 /**
+ *
  *
  * @export
  * @interface Customer
@@ -112,8 +149,10 @@ export interface Customer {
 }
 
 /**
+ * Action Operation Type
+ *
  * @export
- * @enum {string}
+ * @enum {number}
  */
 export enum OperationType {
   Create = "create",
@@ -123,7 +162,9 @@ export enum OperationType {
 }
 
 /**
- * ActivityRelationInstance
+ *
+ *
+ * @interface ActivityRelationInstance
  */
 interface ActivityRelationInstance {
   identifier_value: string | number;
@@ -147,6 +188,8 @@ interface ActivityRelation {
 }
 
 /**
+ *
+ *
  * @export
  * @interface Activity
  */
@@ -163,9 +206,93 @@ export interface Activity {
   relations?: ActivityRelation[];
   currency?: string;
   monetary_value?: unknown;
-  activity_date?: Date | string;
+  activity_date?: string;
   tags?: RequireAtLeastOne<Omit<Tag, "description">>[];
   readonly created_at: string;
   readonly updated_at: string;
   operation_type?: OperationType;
+}
+
+/**
+ *
+ * @export
+ * @interface ActionTypeGroup
+ */
+export interface ActionTypeGroup {
+  readonly slug: string;
+  title: string;
+  description?: string;
+  default_text_template?: ObjectData;
+}
+
+/**
+ *
+ *
+ * @export
+ * @interface ActionType
+ */
+export interface ActionType {
+  title: string;
+  description?: string | null;
+  /**
+   * integration slug on which this action type is created
+   *
+   * @type {string}
+   * @memberof ActionType
+   */
+  integration: string;
+  /**
+   * an array of tag slugs
+   *
+   * @type {string[]}
+   * @memberof ActionType
+   */
+  tags: string[];
+  /**
+   *
+   * @type {string}
+   * @memberof ActionType
+   */
+  readonly slug: string;
+  /**
+   *
+   * @type {Date}
+   * @memberof ActionType
+   */
+  readonly created_at: string;
+  /**
+   *
+   * @type {Date}
+   * @memberof ActionType
+   */
+  readonly updated_at: string;
+  /**
+   * shows the default operation type for the activities with this action type.
+   * @type {OperationType}
+   * @memberof ActionType
+   */
+  default_operation_type?: OperationType;
+  /**
+   * add a series of text templates for the activities with this action type.
+   * for example {"en": "An order with was the order code {order_code} was created."}
+   *
+   * @type {ObjectData}
+   * @memberof ActionType
+   */
+  activity_text_template?: ObjectData;
+  /**
+   * how much score will a customer gain by performing an
+   * activity with this action type? (as defined by action type owner.)
+   *
+   * @type {number}
+   * @memberof ActionType
+   */
+  action_score?: number;
+  /**
+   * action type group slug
+   *
+   * @type {string}
+   * @memberof ActionType
+   */
+  group?: string;
 }
